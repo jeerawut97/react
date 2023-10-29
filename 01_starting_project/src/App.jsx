c51616501189128f0d18f5d8446ed0b5dd238ada
JRW <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1@github.com>
2023-10-29 14:44:17 +0700

    Implement Project Structure

--- 01_starting_project/src/App.jsx
+++ 01_starting_project/src/App.jsx
@@ -1,58 +1,8 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
-import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import Main from './Main.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandonInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function Header() {
-  const description = reactDescriptions[genRandonInt(reactDescriptions.length)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title}/>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function Main() {
-  return (
-    <main>
-      <section id="core-concepts">
-        <h2>Time to get started!</h2>
-        <ul>
-          <CoreConcept
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
-          />
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-        </ul>
-      </section>
-    </main>
-  )
-}
-
-function App() {
+export default function App() {
   return (
     <div>
       <Header></Header>
@@ -60,5 +10,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
---
+++ 01_starting_project/src/Main.jsx
@@ -0,0 +1,23 @@
+import CoreConcept from './components/CoreConcept.jsx';
+import { CORE_CONCEPTS } from './data.js';
+
+
+export default function Main() {
+    return (
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+      </main>
+    )
+  }
