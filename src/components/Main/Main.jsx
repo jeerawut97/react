7c08e00335e0943bd1917e9b053475cfd152c814
JRW <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1@github.com>
2023-10-29 17:38:36 +0700

    passing custom arguments to event function

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,8 +4,8 @@ import { CORE_CONCEPTS } from '../../data.js';
 
 
 export default function Main() {
-    function handleSelect () {
-      console.log("selected!")
+    function handleSelect(selectedButton) {
+      console.log(selectedButton)
     }
 
     return (
@@ -26,10 +26,10 @@ export default function Main() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           Dynamic Content
         </section>
