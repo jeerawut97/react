6c8b0933f557d73d7488df21394b275180c52833
JRW <jeerawut.jd40@gmail.com>
2023-11-04 16:21:23 +0700

    Rendering Content Conditionally

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,10 +6,22 @@ import { CORE_CONCEPTS, EXAMPLES } from '../../data.js';
 
 
 export default function Main() {
-    const [ selectedTopic, setSelectedTopic ] = useState('components');
-
+    const [ selectedTopic, setSelectedTopic ] = useState();
     function handleSelect(selectedTopic) {
       setSelectedTopic(selectedTopic);
+    }
+
+    let tabContent = <p>Please select a topic.</p>
+    if (selectedTopic) {
+      tabContent = (
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
+      );
     }
 
     return (
@@ -35,13 +47,7 @@ export default function Main() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     )
