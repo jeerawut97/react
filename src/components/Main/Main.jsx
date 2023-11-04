630fdf1c9413299af7e0038f6b84cf7f527a9394
JRW <jeerawut.jd40@gmail.com>
2023-11-04 15:56:21 +0700

    Outputting Data Based on State

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 
 import CoreConcept from '../CoreConcept/CoreConcept.jsx';
 import TabButton from '../TabButton/TabButton.jsx';
-import { CORE_CONCEPTS } from '../../data.js';
+import { CORE_CONCEPTS, EXAMPLES } from '../../data.js';
 
 
 export default function Main() {
-    const [ selectedTopic, setSelectedTopic ] = useState('Please click a button!');
+    const [ selectedTopic, setSelectedTopic ] = useState('components');
 
     function handleSelect(selectedTopic) {
       setSelectedTopic(selectedTopic);
@@ -35,7 +35,13 @@ export default function Main() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     )
